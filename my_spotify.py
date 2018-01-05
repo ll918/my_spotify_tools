c3e373a925390f3decb15d899c8e1f623905ffbf
ll918 <ll918@users.noreply.github.com>
2018-01-05 17:18:41 -0500

    Added conditions after requests.

--- my_spotify.py
+++ my_spotify.py
@@ -6,6 +6,8 @@ https://beta.developer.spotify.com/documentation/general/guides/scopes/
 https://beta.developer.spotify.com/documentation/web-api/reference/search/search/
 https://beta.developer.spotify.com/documentation/general/guides/working-with-playlists/
 https://beta.developer.spotify.com/documentation/web-api/reference/playlists/replace-playlists-tracks/
+
+https://beta.developer.spotify.com/documentation/web-api/#response-status-codes
 """
 import json
 import os
@@ -53,8 +55,8 @@ def get_original_tokens_with_code():
                'redirect_uri': redirect_uri}
 
     r = common.post_request(endpoint, headers, payload)
-    d = r.json()
-    if d:
+    if r.status_code == 200:
+        d = r.json()
         print(d)
     return
 
@@ -69,6 +71,7 @@ def refresh_token():
     payload = {'grant_type': 'refresh_token',
                'refresh_token': rtoken}
     r = common.post_request(endpoint, headers, payload)
+    # if r.status_code == 200:
     d = r.json()
     return d['access_token']
 
@@ -93,34 +96,39 @@ def get_spotify_track_data(title, data, token):
                  json.dumps(track), '"&type=track&limit=1'])
             url = search_endpoint + query
             r = common.get_request(url, {'Authorization': 'Bearer ' + token})
-            d = r.json()
-            if d['tracks']['total'] > 0:
-                album_info = d['tracks']['items'][0]
-                album_name = album_info['album']['name']
-                album_id = album_info['album']['id']
-                album_url = album_info['album']['external_urls']['spotify']
+            if r.status_code == 200:
+                d = r.json()
+                if d['tracks']['total'] > 0:
+                    album_info = d['tracks']['items'][0]
+                    album_name = album_info['album']['name']
+                    album_id = album_info['album']['id']
+                    album_url = album_info['album']['external_urls']['spotify']
 
-                artist_info = album_info['artists'][0]
-                artist_name = artist_info['name']
-                artist_id = artist_info['id']
-                artist_url = artist_info['external_urls']['spotify']
+                    artist_info = album_info['artists'][0]
+                    artist_name = artist_info['name']
+                    artist_id = artist_info['id']
+                    artist_url = artist_info['external_urls']['spotify']
 
-                track_name = album_info['name']
-                track_id = album_info['id']
-                track_uri = album_info['uri']
-                track_url = album_info['external_urls']['spotify']
+                    track_name = album_info['name']
+                    track_id = album_info['id']
+                    track_uri = album_info['uri']
+                    track_url = album_info['external_urls']['spotify']
 
-                tracks_data[track_id] = {'track_name': track_name,
-                                         'track_url': track_url,
-                                         'track_uri': track_uri,
-                                         'album': {'album_id': album_id,
-                                                   'album_name': album_name,
-                                                   'album_url': album_url},
-                                         'artist': {'artist_id': artist_id,
-                                                    'artist_name': artist_name,
-                                                    'artist_url': artist_url}}
+                    tracks_data[track_id] = {'track_name': track_name,
+                                             'track_url': track_url,
+                                             'track_uri': track_uri,
+                                             'album': {'album_id': album_id,
+                                                       'album_name': album_name,
+                                                       'album_url': album_url},
+                                             'artist': {'artist_id': artist_id,
+                                                        'artist_name': artist_name,
+                                                        'artist_url': artist_url}}
+                else:
+                    not_found.append(i)
+
             else:
-                not_found.append(i)
+                print('There was a problem with the request')
+                print(r)
 
     if tracks_data:
         common.save_to_json(tracks_data, './json/' + title + '_data.json')
@@ -150,9 +158,15 @@ def create_playlist(title, token):
     payload = json.dumps({'name': title, 'public': 'false'})
 
     r = common.post_request(endpoint, headers, payload)
-    d = r.json()
-    playlist_id = d['id']
-    print('Playlist', title, 'created')
+    playlist_id = ''
+    if r.status_code == 201:
+        d = r.json()
+        playlist_id = d['id']
+        print('Playlist', title, 'created')
+    else:
+        print(r)
+        print(r.status_code)
+
     return playlist_id
 
 
@@ -170,7 +184,11 @@ def add_track_playlist(playlist_id, d, token):
         tracks_uri.append(v['track_uri'])
     payload = json.dumps({"uris": tracks_uri})
     r = common.post_request(endpoint, headers, payload)
-    print('tracks added to playlist')
+    if r.status_code == 201:
+        print('tracks added to playlist')
+    else:
+        print('There was a problem with the request')
+        print(r)
     return
 
 # get_user_authorization_and_code()
